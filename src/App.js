94e3273a306dc243fcceb99ecf0c31314953c4ea
Wajahat-404 <109886908+Wajahat-404@users.noreply.github.com>
2023-07-07 16:27:21 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Error from './components/Error';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div className="App">
+
+        <Header />
+        {/* <p>Currently, You're using header and inside the Home.</p> */}
+
+        <Routes>
+          {/* Home */}
+        <Route path='/' element={<Home />} />
+        {/* About */}
+        <Route path='/about' element={<About />} />
+        {/* Contact */}
+        <Route path='/contact' element={<Contact />} />
+        {/* Error */}
+        <Route path='*' element={<Error />} />
+        </Routes>
+        
     </div>
+    </Router>
   );
 }
 
---
+++ src/components/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+        <h2>I'm Contact.</h2>
+    </div>
+  )
+}
+
+export default Contact
