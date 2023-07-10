3e5ab3d416aee1d31de80a66df30122a60131394
Wajahat-404 <109886908+Wajahat-404@users.noreply.github.com>
2023-07-10 17:31:03 +0500

    added extra links

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,6 +7,8 @@ const Contact = () => {
         <h2>I'm Contact.</h2>
         <Link to={'insta'}> <h4>Connect via Insta</h4></Link>
         <Link to={'mail'}> <h4>Connect via Mail</h4></Link>
+        <Link to={'twitter'}> <h4>Connect via Twitter</h4></Link>
+        <Link to={'threads'}> <h4>Connect via Threads</h4></Link>
         <Outlet />
     </div>
   )
